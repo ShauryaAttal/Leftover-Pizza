75d7920098460afadc723eeb979411f7f761678b
ShauryaAttal <87157875+ShauryaAttal@users.noreply.github.com>
2021-07-08 20:21:06 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+
+var a = prompt('Enter the first variable: ');
+var b = prompt('Enter the second variable: ');
+function setup(){
+  var b2 = createButton("click here to Swap");
+  b2.mousePressed(swap);
+  }
+
+
+function draw() {
+background(220);
+}
+
+function swap() {
+[a,b] = [b,a];
+console.log('Value of a after swap is:' + a);
+console.log('Value of b after swap is:' + b);
+}
+
+
